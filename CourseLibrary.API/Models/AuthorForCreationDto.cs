22e2b77c4597c530bacacb39c9725bb9ab39238d
Ryan Wire <ryanwire@outlook.com>
2021-03-23 01:39:49 +0300

    Adds functionality for creating a child resource with a parent resource

--- CourseLibrary.API/Models/AuthorForCreationDto.cs
+++ CourseLibrary.API/Models/AuthorForCreationDto.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace CourseLibrary.API.Models
 {
     public class AuthorForCreationDto
@@ -7,5 +9,6 @@ namespace CourseLibrary.API.Models
         public string LastName { get; set; }
         public DateTimeOffset DateOfBirth { get; set; }
         public string MainCategory { get; set; }
+        public ICollection<CourseForCreationDto> Courses { get; set; } = new List<CourseForCreationDto>();
     }
 }

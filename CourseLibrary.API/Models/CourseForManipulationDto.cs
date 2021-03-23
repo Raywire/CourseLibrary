2247f5f9f05099c1297c977bf248583fcca7f1a2
Ryan Wire <ryanwire@outlook.com>
2021-03-23 19:51:34 +0300

    Validates input when updating a resource with PUT

---
+++ CourseLibrary.API/Models/CourseForManipulationDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using CourseLibrary.API.ValidationAttributes;
+
+namespace CourseLibrary.API.Models
+{
+    [CourseTitleMustBeDifferentFromDescriptionAttribute(ErrorMessage = "Title must be different from description.")]
+    public abstract class CourseForManipulationDto
+    {
+        [Required(ErrorMessage = "You should fill out a title.")]
+        [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
+        public string Title { get; set; }
+
+        [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
+        public virtual string Description { get; set; }
+        // abstract means the Description member must be implemented in classes that derive from abstract class (used if there is no default implementation of the property)
+        // virtual - used if there is a default implementation, and allows overriding
+    }
+}

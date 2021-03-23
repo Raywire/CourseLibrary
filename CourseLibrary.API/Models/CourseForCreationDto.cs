80cf08740aeb55da37351d5740cea6ce1f5ffdf3
Ryan Wire <ryanwire@outlook.com>
2021-03-23 18:40:53 +0300

    Sets up class level input validation with custom attribute

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using CourseLibrary.API.ValidationAttributes;
 
 namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto : IValidatableObject
+    [CourseTitleMustBeDifferentFromDescriptionAttribute]
+    public class CourseForCreationDto //: IValidatableObject
     {
         [Required]
         [MaxLength(100)]
@@ -13,13 +15,13 @@ namespace CourseLibrary.API.Models
         [MaxLength(1500)]
         public string Description { get; set; }
 
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (Title == Description)
-            {
-                yield return new ValidationResult("The provided description should be different from the title.",
-                new[] { "CourseForCreationDto" });
-            }
-        }
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult("The provided description should be different from the title.",
+        //        new[] { "CourseForCreationDto" });
+        //    }
+        //}
     }
 }

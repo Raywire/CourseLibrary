1be06855eef7c8fcfec84c423b8afbb2efd67aba
Ryan Wire <ryanwire@outlook.com>
2021-03-24 20:49:12 +0300

    Tightens the contract between client and server vendor-specific media types

--- CourseLibrary.API/Profiles/AuthorsProfile.cs
+++ CourseLibrary.API/Profiles/AuthorsProfile.cs
@@ -17,6 +17,8 @@ namespace CourseLibrary.API.Profiles
                     opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge()));
 
             CreateMap<Models.AuthorForCreationDto, Entities.Author>();
+
+            CreateMap<Entities.Author, Models.AuthorFullDto>();
         }
     }
 }
